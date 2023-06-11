436e95f9d3c3d0415e3d546520c71e29254f7859
Anthony-4926 <2936616693@qq.com>
2023-06-11 11:00:55 +0800

    广播获取电量，动态更新显示

--- app/src/main/java/com/example/batteryinfo/Pow.java
+++ app/src/main/java/com/example/batteryinfo/Pow.java
@@ -2,39 +2,44 @@ package com.example.batteryinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
-import android.os.BatteryManager;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
-
-import java.util.Calendar;
 
 public class Pow extends AppCompatActivity {
 
     private TextView batteryLevelTextView;
+    int pow;
+    //创建BroadcastReceiver
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pow);
-
+        getBatteryPercentage();
         batteryLevelTextView = findViewById(R.id.textView2);
-        int pow = getBatteryPercentage();
-        if(pow < 20) {
-            batteryLevelTextView.setTextColor(Color.RED);
-        }else{
-            batteryLevelTextView.setTextColor(Color.GREEN);
-        }
-        batteryLevelTextView.setText(""+pow);
     }
 
-    public int getBatteryPercentage() {
-        BatteryManager batteryManager = (BatteryManager) Pow.this.getSystemService(Pow.BATTERY_SERVICE);
-        int pow = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
-        System.out.println(pow);
-        return pow;
+    public void getBatteryPercentage() {
+        BroadcastReceiver receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                pow = intent.getIntExtra("level", -1);
+                if(pow < 20) {
+                    batteryLevelTextView.setTextColor(Color.RED);
+                }else{
+                    batteryLevelTextView.setTextColor(Color.GREEN);
+                }
+                batteryLevelTextView.setText(""+pow);
+            }
+        };
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(receiver, intentFilter);
 
     }
 
